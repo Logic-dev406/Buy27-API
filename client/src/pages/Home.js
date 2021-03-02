999ae816723b107c35adc11e593b28d4773ddd74
patrick sunday <sundayblinkz1@gmail.com>
2021-03-02 22:54:35 +0100

    made infor banner mobile  responsive

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,12 +10,12 @@ const Home = () => {
             <div className="border-b-2 border-gray mx-0 md:mx-20 mb-4 md:mb-8"></div>
             <FeaturedProduct />
             <div className="flex flex-col items-center">
-                <div className=" h-64 w-full md:w-2/3 mb-4 md:mb-8 bg-primary-dark text-white">
-                    <h1 className="text-center mt-8 text-lg font-normal">
+                <div className="  w-full md:w-2/3 mb-4 md:mb-8 bg-primary-dark text-white">
+                    <h1 className="text-center mt-8 text-sm mx-2 md:mx-0 md:text-lg font-normal">
                         Online Shopping on Buy27.com – Nigeria’s Largest Online
                         Mall
                     </h1>
-                    <h1 className="text-center my-2 mx-8 font-light">
+                    <h1 className="text-center my-2 mx-4 md:mx-8 text-sm md:text-lg font-light">
                         Buy27.com is Nigeria’s number one online Shopping
                         destination.We pride ourselves in having everything you
                         could possibly need for life and living at the best
@@ -27,10 +27,10 @@ const Home = () => {
                         Building and construction materials and a whole lot more
                         from premium brands.
                     </h1>
-                    <h1 className="text-center text-normal font-normal">
+                    <h1 className="text-center text-sm md:text-normal font-normal">
                         Mr.Sunday Patrick
                     </h1>
-                    <h1 className="text-center text-xs font-light">
+                    <h1 className="text-center text-xs mb-2 md:mb-2 font-light">
                         Buy27 Manager
                     </h1>
                 </div>
