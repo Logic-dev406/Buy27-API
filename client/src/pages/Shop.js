5c3fe669849b6933292a2a3c36848a10395c23be
patrick sunday <sundayblinkz1@gmail.com>
2021-03-11 09:23:49 +0100

    refactor the shop code

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,53 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+import ShopProduct from '../components/Shop/ShopProducts';
 
 const Shop = () => {
-    useEffect(() => {
-        fechItems();
-    }, []);
-
-    const [items, setItems] = useState([]);
-
-    const fechItems = async () => {
-        const response = await axios.get('http://localhost:3000/api/products');
-
-        console.log(response.data);
-        setItems(response.data);
-    };
-
     return (
         <div className="flex flex-col bg-gray-100 h">
             <div className="flex justify-center mx-52">
                 <div className="h-96 w-72 bg-white mr-5"></div>
-                <div className="  bg-white">
-                    <div className=" grid grid-cols-2 md:grid-cols-4 gap-y-0 md:gap-y-2 gap-x-1 md:gap-x-2 py-4 px-4 bg-transparent">
-                        {items.map((item) => (
-                            <div
-                                key={item.id}
-                                className=" bg-white * hover:shadow-lg h-72 md:h-80 w-40 md:w-48 rounded justify-center"
-                            >
-                                <img
-                                    className="h-48 w-48 rounded-t"
-                                    src={item.image}
-                                    alt="product"
-                                />
-                                <h1 className="text-left text-black text-xs font-semibold pl-2 pt-2">
-                                    {item.name}
-                                </h1>
-
-                                <h5 className="text-left text-gray-500 text-xs pl-2 font-semibold">
-                                    <span className="mr-1">&#8358;</span>
-                                    {item.price}
-                                </h5>
-                                <button className="mt-10 mx-2 bg-transparent hover:bg-primary-dark rounded border text-primary-dark hover:text-white border-primary-dark w-44 h-10">
-                                    <h1 className=" text-sm font-bold">
-                                        Add To Cart
-                                    </h1>
-                                </button>
-                            </div>
-                        ))}
-                    </div>
-                </div>
+                <ShopProduct />
             </div>
         </div>
     );
