1a6cd9917307a459919d0ce92a8baabdf5e4451c
patrick sunday <sundayblinkz1@gmail.com>
2021-03-11 09:56:15 +0100

    added scroll to the top to the mobile footer

--- client/src/sections/Footer/MobileFooter.js
+++ client/src/sections/Footer/MobileFooter.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 
 const MobileFooter = () => {
+    function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+
     return (
-        <div className="items-center bg-primary-dark mt-4  w-full py-4 px-2 text-white">
+        <div className="items-center bg-primary-dark mt-4  w-full pb-4 pt-1 px-2 text-white">
+            <div onClick={topFunction} className="flex flex-col items-center">
+                <ExpandLessIcon fontSize="small" />
+                <h1 className="text-xs font-light">BACK TO TOP</h1>
+            </div>
+            <div className="border-b border-primary-light my-2"></div>
             <div className="flex justify-center text-xs font-light ">
                 <a href="/" className="px-2">
                     Help Center
@@ -22,7 +34,7 @@ const MobileFooter = () => {
                     Contact Us
                 </a>
             </div>
-            <div className="border-b border-primary-light my-4"></div>
+            <div className="border-b border-primary-light my-2"></div>
             <div>
                 <p className="flex justify-center text-xs font-light ">
                     Copyright &copy; {new Date().getFullYear()} Buy27.com.
