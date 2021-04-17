82e8f6cd34b915725c0288bb9afe91f7a74461ef
patrick sunday <sundayblinkz1@gmail.com>
2021-04-17 19:09:50 +0100

    added form validation to update address page

--- client/src/helper/validator.js
+++ client/src/helper/validator.js
@@ -13,6 +13,22 @@ export default function validateInfo(values) {
         errors.mobilenumber = 'Mobile Number required';
     }
 
+    if (!values.state) {
+        errors.state = 'State is required';
+    }
+
+    if (!values.lga) {
+        errors.lga = ' Local Goverment Area is required';
+    }
+
+    if (!values.city) {
+        errors.city = 'City is required';
+    }
+
+    if (!values.streetaddress) {
+        errors.streetaddress = 'Street Address is required';
+    }
+
     if (!values.emailaddress) {
         errors.emailaddress = 'Email required';
     } else if (!/\S+@\S+\.\S+/.test(values.emailaddress)) {
