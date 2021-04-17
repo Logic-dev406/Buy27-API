6831ce4a25565b7a8bfc285d235c03a824b04c83
patrick sunday <sundayblinkz1@gmail.com>
2021-04-17 12:54:54 +0100

    made forgot password page mobile responsive

--- client/src/pages/ForgotPassword.js
+++ client/src/pages/ForgotPassword.js
@@ -25,16 +25,18 @@ export const ForgotPassword = () => {
     return (
         <div className=" flex flex-col items-center justify-center h-screen w-full bg-gray-100">
             <div className=" flex flex-col items-center pt-4 pb-8 bg-white shadow-lg rounded">
-                <h1 className=" text-3xl font-bold mb-2">Forgot Password</h1>
+                <h1 className=" text-xl md:text-3xl font-bold mb-2">
+                    Forgot Password
+                </h1>
                 <div className="  border-b border-gray w-full my-2"></div>
-                <div className=" flex flex-col items-center justify-center text-center text-sm h-28 w-96 px-4 bg-gray-100">
+                <div className=" flex flex-col items-center justify-center text-center text-sm h-28 w-72 md:w-96 px-4 bg-gray-100">
                     <h1>
                         Please enter the e-mail address associated with your
                         buy27 account. We will send you a link to this e-mail
                         address to reset your password.
                     </h1>
                 </div>
-                <div className="flex flex-col px-8 mt-2 mb-4">
+                <div className="flex flex-col px-4 md:px-8 mt-2 mb-4">
                     <div className="text-sm text-primary-dark mb-2">
                         <label htmlFor="emailaddress ">Email Address</label>
                     </div>
@@ -45,7 +47,7 @@ export const ForgotPassword = () => {
                         name="emailaddress"
                         value={values.emailaddress}
                         placeholder="Enter Email Address"
-                        className="focus: outline-none bg-transparent border border-primary-dark rounded px-4 h-12 w-96"
+                        className="focus: outline-none bg-transparent border border-primary-dark rounded px-4 h-10 md:h-12 w-72 md:w-96"
                     />
                     {errors.emailaddress && (
                         <p className="text-red-500 text-sm ">
@@ -55,7 +57,7 @@ export const ForgotPassword = () => {
                 </div>
                 <button
                     onClick={handleSubmit}
-                    className="h-10 px-44 bg-primary-dark hover:bg-primary-light focus:outline-none text-white rounded"
+                    className="h-10 px-32 md:px-44 bg-primary-dark hover:bg-primary-light focus:outline-none text-white rounded"
                 >
                     Login
                 </button>
@@ -63,7 +65,7 @@ export const ForgotPassword = () => {
                     <h1 className="text-sm">I remember my password?</h1>
                     <Link
                         to="/login"
-                        className="flex items-center h-10 px-44 mt-2 bg-transparent hover:bg-primary-dark border border-primary-dark no-underline font-semibold text-primary-dark hover:text-white rounded"
+                        className="flex items-center h-10 px-32 md:px-44 mt-2 bg-transparent hover:bg-primary-dark border border-primary-dark no-underline font-semibold text-primary-dark hover:text-white rounded"
                     >
                         <h1>Login</h1>
                     </Link>
