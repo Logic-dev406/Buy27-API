0e05d3497407de2b3a43400d9cd23a07390504d0
patrick sunday <sundayblinkz1@gmail.com>
2021-08-06 23:51:50 +0100

    updated category controller

--- controllers/CategoriesController.js
+++ controllers/CategoriesController.js
@@ -3,68 +3,88 @@ const response = require('../helpers/response');
 
 class CategoriesController {
     static async getCategories(req, res) {
-        const categoryList = await Category.find();
+        try {
+            const categoryList = await Category.find();
 
-        if (!categoryList) {
-            res.status(500).send(response('Category not found', {}, false));
+            if (!categoryList) {
+                res.status(500).send(response('Category not found', {}, false));
+            }
+
+            res.status(200).send(
+                response('Fetched categories successfully', categoryList)
+            );
+        } catch (error) {
+            console.log(error.message);
         }
-
-        res.status(200).send(
-            response('Fetched categories successfully', categoryList)
-        );
     }
 
     static async getCategoryById(req, res) {
-        const category = await Category.findById(req.params.id);
+        try {
+            const category = await Category.findById(req.params.id);
 
-        if (!category) {
-            return res
-                .status(500)
-                .send(
-                    response(
-                        'Category with the given ID was not found',
-                        {},
-                        false
-                    )
-                );
+            if (!category) {
+                return res
+                    .status(500)
+                    .send(
+                        response(
+                            'Category with the given ID was not found',
+                            {},
+                            false
+                        )
+                    );
+            }
+
+            res.status(200).send(
+                response('Fetched category successfully', category)
+            );
+        } catch (error) {
+            console.log(error.message);
         }
-
-        res.status(200).send(
-            response('Fetched category successfully', category)
-        );
     }
 
     static async createCategory(req, res) {
-        let category = new Category({
-            name: req.body.name,
-            icon: req.body.icon,
-        });
-        category = await category.save();
+        try {
+            let category = new Category({
+                name: req.body.name,
+                icon: req.body.icon,
+            });
+            category = await category.save();
 
-        if (!category)
-            return res
-                .status(404)
-                .send(response('The category can not be created', {}, false));
+            if (!category)
+                return res
+                    .status(404)
+                    .send(
+                        response('The category can not be created', {}, false)
+                    );
 
-        res.send(response('Category created successfully', category));
+            res.send(response('Category created successfully', category));
+        } catch (error) {
+            console.log(error.message);
+        }
     }
 
     static async updateCategoryById(req, res) {
-        const category = await Category.findByIdAndUpdate(
-            req.params.id,
-            {
-                name: req.body.name,
-                icon: req.body.icon,
-            },
-            { new: true }
-        );
+        try {
+            const category = await Category.findByIdAndUpdate(
+                req.params.id,
+                {
+                    name: req.body.name,
+                    icon: req.body.icon,
+                },
+                { new: true }
+            );
 
-        if (!category)
-            return res
-                .status(404)
-                .send(response('The category can not be updated', {}, false));
+            if (!category)
+                return res
+                    .status(404)
+                    .send(
+                        response('The category can not be updated', {}, false)
+                    );
 
-        res.send(response('Category was successfully updated', category));
+            res.send(response('Category was successfully updated', category));
+        } catch (error) {
+            console.log(error.message);
+        }
     }
 
     static deleteCategoryById(req, res) {
